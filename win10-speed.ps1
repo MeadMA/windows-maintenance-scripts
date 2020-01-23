94b9944369065c5da41dfca46fcfbf49df2d04ce
Michael A. Mead <michael.a.mead@gmail.com>
2020-01-22 21:35:41 -0500

    Moved license notice above help information in win10-speed.ps1

--- win10-speed.ps1
+++ win10-speed.ps1
@@ -1,5 +1,21 @@
 #Requires -RunAsAdministrator
 
+# win10-speed: Improves the speed of Windows 10
+# Copyright (C) 2020  Michael A. Mead
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 <#
 .SYNOPSIS
 Provides options to speed up Windows 10.
@@ -34,22 +50,6 @@ https://www.gnu.org/licenses/
 .LINK
 https://github.com/MeadMA/windows-maintenance-scripts/
 #>
-
-# win10-speed: Improves the speed of Windows 10
-# Copyright (C) 2020  Michael A. Mead
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ## Program starts below here.
 
